47562206c6d53e49c2cfdc8511f2f9ac8b16fd36
Eloquence98 <ibneenayat1998@gmail.com>
2022-07-19 07:25:13 +0500

    add variables.js install dependencies add goals section (undecided about images state and also swiper.js not working yet) put some code inside consts in js file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bP = require("body-parser");
 const ejs = require("ejs");
+const variables = require(__dirname +"/variables.js");
+
 
 const app = express();
 // port
@@ -34,7 +36,7 @@ app.use(express.static("public"));
 // include this when about to diploy = process.env.PORT ||
 // listening for/from home
 app.get('/', (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs" , {goalsArticles: variables.goalsArticles});
 })
 
 // app shall listen to port 3000
---
+++ variables.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// **************************
+// Generate and export Home section variables
+// **************************
+
+exports.goalsArticles = [
+  {
+    name: "Community",
+    nameState: "active-btn",
+    article: `We strongly believe that no other NFT community is as close and friendly as
+       this one. It makes
+       sense.
+       Communities with 10,000+ members will never be able to be as close as
+       communities with only 333
+       members.
+       Join our Discord or Telegram servers and have a chat with us :)`,
+     articleState: "active-paragraph"
+  },
+  {
+   name: "Future",
+   article: `We know that the future of NFTs is not just about posting a new profile
+   pictures on Twitter. We are
+   familiar with metaverse, virtual realities and much more. Year 2022 is going
+   to get our collection
+   to a
+   whole different level. Connecting Metaverse and NFTs with children education
+   is on our list as well
+   as
+   NFT
+   staking and other rewards!`,
+ },
+];
